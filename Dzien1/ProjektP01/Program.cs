80d56f78e07b22c63498b5a79a79875c18a7d60d
PDD3V <p.domeradzki@wp.pl>
2022-10-23 14:06:54 +0200

    Initial commit

---
+++ Dzien1/ProjektP01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektP01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Clc k = new Clc();
+
+            k.Rozmiar ="duży";
+
+            int wyn = k.Dodaj(1, 2);
+
+            Console.WriteLine(wyn);
+            Console.ReadKey();
+        }
+    }
+}
