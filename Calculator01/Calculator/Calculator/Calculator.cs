440e1e5c7f2863b27ec6a1fdb5ca7b1619d0a871
PDD3V <p.domeradzki@wp.pl>
2022-10-28 22:07:57 +0200

    GUI and Res
    Added GUI to project and number button click event

---
+++ Calculator01/Calculator/Calculator/Calculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+            Result.Text ="0";
+        }
+      
+        private void OnNumberClick(object sender, EventArgs e)
+        {
+            var clickedValue = (sender as Button).Text;
+
+            if (Result.Text == "0")
+                Result.Text = String.Empty; 
+
+           Result.Text += clickedValue;
+        }
+
+        private void OnButtonOperation(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnButtonResult(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnButtonClear(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
