83018c69577a41b5deb3fc9993d187e6b4f90a95
PDD3V <p.domeradzki@wp.pl>
2022-11-20 13:02:22 +0100

    DB2

---
+++ BD02/DB/Projekt1DB/Models/Osoby.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Projekt1DB.Models;
+
+public partial class Osoby
+{
+    public int Id { get; set; }
+
+    public string? Imie { get; set; }
+
+    public string? Nazwisko { get; set; }
+
+    public int? Wzrost { get; set; }
+
+    public decimal? Waga { get; set; }
+
+    public DateTime? DataUr { get; set; }
+}
---
+++ DB01/DB_Csh/DB_Csh/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Data.OleDb;
+using System.Configuration;
+using System.Data.Odbc;
+using System.Configuration;
+
+namespace DB_Csh
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterName_Click(object sender, RoutedEventArgs e)
+        {
+            using (OdbcConnection con = new OdbcConnection())
+            {
+                con.ConnectionString=ConfigurationManager.ConnectionStrings["Connetion"].ToString();
+                con.Open();
+            }
+            using (OdbcCommand cmd = new OdbcCommand()
+            {
+                cmd.CommandText = "select * from [Employee]";
+            cmd.Connetion = Configuration;
+            ]
+
+        }
+
+
+
+    }
+}
