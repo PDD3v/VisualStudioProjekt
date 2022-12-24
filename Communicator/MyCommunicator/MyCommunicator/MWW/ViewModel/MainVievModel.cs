92d523a722e2a16b71b0fdd1fbf588c20c244e4c
PDD3V <p.domeradzki@wp.pl>
2022-12-24 11:06:28 +0100

    Final version

--- Communicator/MyCommunicator/MyCommunicator/MWW/ViewModel/MainVievModel.cs
+++ Communicator/MyCommunicator/MyCommunicator/MWW/ViewModel/MainVievModel.cs
@@ -1,12 +1,123 @@
-﻿using System;
+﻿using MyCommunicator.Core;
+using MyCommunicator.MWW.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyCommunicator.MWW.ViewModel
 {
-    internal class MainVievModel
+     class MainVievModel : ObservableObject
     {
+        public ObservableCollection<MessageModel> Messages { get; set; }
+        public ObservableCollection<ContactModel> Contacts { get; set; }
+
+        /*Commands*/
+
+        public RelayCommand SendCommand { get; set; }
+
+        private ContactModel _selectedContact;
+
+        private ContactModel SelectedContact
+        {
+            get { return _selectedContact; }
+            set
+            { 
+                _selectedContact = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private string _message;
+
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                _message = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainVievModel()
+        {
+            Messages= new ObservableCollection<MessageModel>();
+            Contacts= new ObservableCollection<ContactModel>();
+
+            SendCommand = new RelayCommand(o =>
+            {
+                Messages.Add(new MessageModel
+                {
+                    Message = Message,
+                    FirstMessage = false
+                });
+
+                Message = "";
+            });
+
+            Messages.Add(new MessageModel
+            {
+                Username = "Allison",
+                UsernameColor = "409aff",
+                ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                Message = "Test",
+                Time= DateTime.Now,
+                IsNativeOrigin=false,
+                FirstMessage = true
+            });
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Allison",
+                    UsernameColor = "409aff",
+                    ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                    Message = "Test",
+                    Time= DateTime.Now,
+                    IsNativeOrigin=false,
+                    FirstMessage = false
+                });
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Bunny",
+                    UsernameColor = "409aff",
+                    ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                    Message = "Test",
+                    Time= DateTime.Now,
+                    IsNativeOrigin=true
+                });
+            }
+            
+            Messages.Add(new MessageModel
+               {
+                  Username = "Bunny",
+                  UsernameColor = "409aff",
+                  ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                  Message = "Last",
+                  Time= DateTime.Now,
+                  IsNativeOrigin=true
+               });
+            for (int i = 0; i < 5; i++)
+            {
+                Contacts.Add(new ContactModel
+                    {
+                    Username = $"Allison {i}",
+                    ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                    Messages= Messages
+                });
+
+            }
+        }
+
     }
 }
